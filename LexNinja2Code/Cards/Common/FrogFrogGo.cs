7ab9d38202236a872bbd6405fa5670c815801436
iOuter <3155186831@qq.com>
2026-05-19 01:19:03 +0800

    Update FrogFrogGo.cs

--- LexNinja2Code/Cards/Common/FrogFrogGo.cs
+++ LexNinja2Code/Cards/Common/FrogFrogGo.cs
@@ -1,4 +1,5 @@
-﻿using LexNinja2.LexNinja2Code.Api;
+﻿using BaseLib.Utils;
+using LexNinja2.LexNinja2Code.Api;
 using LexNinja2.LexNinja2Code.Api.DynamicVars;
 using LexNinja2.LexNinja2Code.Api.Extensions;
 using LexNinja2.LexNinja2Code.Powers;
@@ -27,48 +28,17 @@ public class FrogFrogGo() : LexNinja2Card(0, CardType.Skill, CardRarity.Common, 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
     {
         NinjaAudio.Play("res://LexNinja2/audio/FrogFrogGo.mp3");
-        if (Ninjutsu())
+        if (Ninjutsu(choiceContext))
         {
             await PlayerCmd.GainEnergy(DynamicVars.Energy.BaseValue, Owner);
         }
-        CardModel cardModel = (
-            await CardSelectCmd.FromHand(
-                prefs: new CardSelectorPrefs(CardSelectorPrefs.ExhaustSelectionPrompt, 1),
-                context: choiceContext,
-                player: base.Owner,
-                filter: null,
-                source: this
-            )
-        ).FirstOrDefault();
-        if (cardModel != null)
+        var card = await CommonActions.SelectSingleCard(this, CardSelectorPrefs.ExhaustSelectionPrompt, choiceContext,
+            PileType.Hand);
+        if (card != null)
         {
-            await CardCmd.Exhaust(choiceContext, cardModel);
+            await CardCmd.Exhaust(choiceContext, card);
         }
     }
-
-    private Boolean Ninjutsu()
-    {
-        if (Owner.Creature.GetPower<FreeNinjutsuPower>() != null)
-        {
-            return true;
-        }
-        if (Owner.Creature.GetPower<Lexkela>() != null)
-        {
-            if (Owner.Creature.GetPower<Lexkela>().Amount >= DynamicVars["Renshu"].BaseValue)
-            {
-                PowerCmd.Apply<Lexkela>(
-                    new ThrowingPlayerChoiceContext(),
-                    Owner.Creature,
-                    -DynamicVars["Renshu"].BaseValue,
-                    Owner.Creature,
-                    this
-                );
-                return true;
-            }
-        }
-        return false;
-    }
-
     protected override void OnUpgrade()
     {
         DynamicVars.Energy.UpgradeValueBy(1);
@@ -79,22 +49,4 @@ public class FrogFrogGo() : LexNinja2Card(0, CardType.Skill, CardRarity.Common, 
     public override string BetaPortraitPath => "beta/FrogFrogGo.png".CardImagePath();
 
     protected override bool ShouldGlowGoldInternal => CanCastNinjutsu();
-
-    private Boolean CanCastNinjutsu()
-    {
-        if (Owner.Creature.GetPower<FreeNinjutsuPower>() != null)
-        {
-            return true;
-        }
-
-        if (Owner.Creature.GetPower<Lexkela>() != null)
-        {
-            if (Owner.Creature.GetPower<Lexkela>().Amount >= DynamicVars["Renshu"].BaseValue)
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
 }

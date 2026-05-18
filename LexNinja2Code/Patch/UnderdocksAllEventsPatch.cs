694d5668777f5d98a1850884c15ce83e666cf905
iOuter <3155186831@qq.com>
2026-05-19 07:27:28 +0800

    Update UnderdocksAllEventsPatch.cs

--- LexNinja2Code/Patch/UnderdocksAllEventsPatch.cs
+++ LexNinja2Code/Patch/UnderdocksAllEventsPatch.cs
@@ -8,8 +8,8 @@ namespace LexNinja2.LexNinja2Code.Patch;
 [HarmonyPatch(typeof(Underdocks), nameof(Underdocks.AllEvents), MethodType.Getter)]
 public static class UnderdocksAllEventsPatch
 {
-    static void Postfix(ref IEnumerable<EventModel> __result)
+    static void Postfix(ref IEnumerable<EventModel> result)
     {
-        __result = __result.Concat([ModelDb.Event<TheSpectre>()]).Distinct();
+        result = result.Concat([ModelDb.Event<TheSpectre>()]).Distinct();
     }
 }

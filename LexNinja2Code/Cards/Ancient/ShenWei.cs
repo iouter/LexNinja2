33949988162cee0965cf219cf75b0d4d584d8fcf
iOuter <3155186831@qq.com>
2026-05-19 06:11:06 +0800

    shenwei

---
+++ LexNinja2Code/Cards/Ancient/ShenWei.cs
@@ -0,0 +1,42 @@
+﻿using BaseLib.Utils;
+using LexNinja2.LexNinja2Code.Api;
+using LexNinja2.LexNinja2Code.Api.DynamicVars;
+using LexNinja2.LexNinja2Code.Api.Extensions;
+using LexNinja2.LexNinja2Code.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+
+namespace LexNinja2.LexNinja2Code.Cards;
+
+public class ShenWei() : LexNinja2Card(3, CardType.Power, CardRarity.Ancient, TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new NinjutsuVar(3), new PowerVar<IntangiblePower>(1), new PowerVar<ShenWeiPower>(1)];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+        [HoverTipFactory.FromPower<IntangiblePower>()];
+    protected override HashSet<CardTag> CanonicalTags => [NinjaTags.Ninjutsu];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        NinjaAudio.Play("res://LexNinja2/audio/ShenWei.mp3");
+        if (Ninjutsu(choiceContext))
+        {
+            await CommonActions.ApplySelf<IntangiblePower>(choiceContext, this);
+        }
+        await CommonActions.ApplySelf<ShenWeiPower>(choiceContext, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        EnergyCost.UpgradeBy(-1);
+    }
+
+    public override string CustomPortraitPath => $"ShenWei_p.png".BigCardImagePath();
+    public override string PortraitPath => $"ShenWei.png".CardImagePath();
+    public override string BetaPortraitPath => $"beta/ShenWei.png".CardImagePath();
+
+    protected override bool ShouldGlowGoldInternal => CanCastNinjutsu();
+}

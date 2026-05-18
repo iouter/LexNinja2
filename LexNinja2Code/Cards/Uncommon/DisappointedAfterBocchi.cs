b639e21e3aee85feb267b3e6cf6348502dc2144b
iOuter <3155186831@qq.com>
2026-05-19 00:51:50 +0800

    Update DisappointedAfterBocchi.cs

--- LexNinja2Code/Cards/Uncommon/DisappointedAfterBocchi.cs
+++ LexNinja2Code/Cards/Uncommon/DisappointedAfterBocchi.cs
@@ -1,4 +1,5 @@
-﻿using LexNinja2.LexNinja2Code.Api;
+﻿using BaseLib.Utils;
+using LexNinja2.LexNinja2Code.Api;
 using LexNinja2.LexNinja2Code.Api.Extensions;
 using LexNinja2.LexNinja2Code.Powers;
 using MegaCrit.Sts2.Core.Commands;
@@ -13,7 +14,7 @@ namespace LexNinja2.LexNinja2Code.Cards;
 public class DisappointedAfterBocchi()
     : LexNinja2Card(2, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<GigantificationPower>(1), new PowerVar<DisappointedPower>(1)];
     public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
     protected override IEnumerable<IHoverTip> ExtraHoverTips =>
         [HoverTipFactory.FromPower<WeakPower>()];
@@ -21,20 +22,8 @@ public class DisappointedAfterBocchi()
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
     {
         NinjaAudio.Play("res://LexNinja2/audio/DisappointedAfterBocchi.mp3");
-        await PowerCmd.Apply<GigantificationPower>(
-            new ThrowingPlayerChoiceContext(),
-            Owner.Creature,
-            1,
-            Owner.Creature,
-            this
-        );
-        await PowerCmd.Apply<DisappointedPower>(
-            new ThrowingPlayerChoiceContext(),
-            Owner.Creature,
-            1,
-            Owner.Creature,
-            this
-        );
+        await CommonActions.ApplySelf<GigantificationPower>(choiceContext, this);
+        await CommonActions.ApplySelf<DisappointedPower>(choiceContext, this);
     }
 
     protected override void OnUpgrade()

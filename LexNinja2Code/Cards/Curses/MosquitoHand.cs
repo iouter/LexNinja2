6083c3775bfae0190129ee048dbe467dfe48d28b
Flimsyyy <785243793@qq.com>
2026-05-24 18:26:14 +0800

    蚊子手现在在消耗堆时不会发出蚊子叫，开局的时候会响一声

--- LexNinja2Code/Cards/Curses/MosquitoHand.cs
+++ LexNinja2Code/Cards/Curses/MosquitoHand.cs
@@ -2,11 +2,12 @@
 using LexNinja2.LexNinja2Code.Api;
 using LexNinja2.LexNinja2Code.Api.Cards;
 using LexNinja2.LexNinja2Code.Api.Extensions;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Rooms;
 
 namespace LexNinja2.LexNinja2Code.Cards.Curses;
 
@@ -20,26 +21,39 @@ public class MosquitoHand() : LexNinja2Card(-1, CardType.Curse, CardRarity.Curse
 
     public override Task AfterCardPlayed(PlayerChoiceContext context, CardPlay cardPlay)
     {
+        if (this.Pile.Type == PileType.Exhaust)
+        {
+            return Task.CompletedTask;
+        }
         if (cardPlay.Card.Owner != Owner)
         {
             return Task.CompletedTask;
         }
-        NinjaAudio.Play("res://LexNinja2/audio/Mosquito2.mp3");
+        NinjaAudio.Play("res://LexNinja2/audio/Mosquito2.mp3", 0.4f);
         return Task.CompletedTask;
     }
 
-    public override Task AfterCardDrawn(
-        PlayerChoiceContext choiceContext,
-        CardModel card,
-        bool fromHandDraw
-    )
+    // public override Task AfterCardDrawn(
+    //     PlayerChoiceContext choiceContext,
+    //     CardModel card,
+    //     bool fromHandDraw
+    // )
+    // {
+    //     if (card != this)
+    //     {
+    //         return Task.CompletedTask;
+    //     }
+    //     NinjaAudio.Play("res://LexNinja2/audio/MosquitoHand.mp3");
+    //     return Task.CompletedTask;
+    // }
+
+    public override async Task AfterRoomEntered(AbstractRoom room)
     {
-        if (card != this)
-        {
-            return Task.CompletedTask;
-        }
+        if (room is not CombatRoom)
+            return;
         NinjaAudio.Play("res://LexNinja2/audio/MosquitoHand.mp3");
-        return Task.CompletedTask;
+        await Cmd.Wait(0.5f);
+        NinjaAudio.Play("res://LexNinja2/audio/Mosquito2.mp3", 0.4f);
     }
 
     public override string CustomPortraitPath => $"MosquitoHand_p.png".BigCardImagePath();

92d7ac3653bfb978372ce610231a16a599ab1e95
Flimsyyy <785243793@qq.com>
2026-05-27 20:13:06 +0800

    同样的移除冗余modify carddraw函数

--- LexNinja2Code/Powers/WildSnakeGodPower.cs
+++ LexNinja2Code/Powers/WildSnakeGodPower.cs
@@ -2,7 +2,6 @@
 using LexNinja2.LexNinja2Code.Api;
 using LexNinja2.LexNinja2Code.Api.Extensions;
 using MegaCrit.Sts2.Core.Entities.Cards;
-using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Models;
@@ -58,11 +57,7 @@ public class WildSnakeGodPower : CustomPowerModel
             }
         }
     }
-
-    public override decimal ModifyHandDraw(Player player, decimal count)
-    {
-        return player != Owner.Player ? count : count + Amount;
-    }
+    
 
     private class Data
     {
